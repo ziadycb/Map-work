619ba424e866ab8864d3536664be930c33ce1d10
SoraDr <jad-yaacoub@hotmail.com>
2020-12-11 14:47:47 +0200

    main

--- Computer-engineering/Computer-engineering.js
+++ Computer-engineering/Computer-engineering.js
@@ -8,12 +8,12 @@ $(window).scroll(function() {
     /* affix after scrolling 100px */
     if ($(document).scrollTop() > 450) {
       $('.navbar').removeClass('bg-transparent');
-      $('.navbar').addClass('bg-dark');
+      $('.navbar').addClass('bg-custom');
       $('.navbar').addClass('affix');
     } 
     else {
       $('.navbar').addClass('bg-transparent');
-      $('.navbar').removeClass('bg-dark');
+      $('.navbar').removeClass('bg-custom');
       $('.navbar').removeClass('affix');
     }
   });
