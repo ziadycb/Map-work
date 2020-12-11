74dc09dedc7b42ff11498731207a1529d60e2bda
ziadycb <Zarayaacoub@hotmail.com>
2020-12-11 02:54:25 +0200

    main

--- Computer-engineering/Computer-engineering.js
+++ Computer-engineering/Computer-engineering.js
@@ -1,10 +1,17 @@
+
+function detectMob() {
+  return ( ( window.innerWidth <= 992 ) );
+}
+
+
 $(window).scroll(function() {
     /* affix after scrolling 100px */
     if ($(document).scrollTop() > 450) {
       $('.navbar').removeClass('bg-transparent');
       $('.navbar').addClass('bg-dark');
       $('.navbar').addClass('affix');
-    } else {
+    } 
+    else {
       $('.navbar').addClass('bg-transparent');
       $('.navbar').removeClass('bg-dark');
       $('.navbar').removeClass('affix');
